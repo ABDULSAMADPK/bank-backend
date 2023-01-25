f26457d1864739dce40d0ac206659aee689b0d90
ABDULSAMADPK <abdulsamadpk9072@gmail.com>
2023-01-25 16:34:07 +0530

    Bank backend

---
+++ services/db.js
@@ -0,0 +1,23 @@
+//import mongoose
+const mongoose = require('mongoose')
+
+
+//using mongoose define connection string
+mongoose.connect('mongodb://localhost:27017/bank',()=>{
+    console.log('MongoDB connected successfully');
+})
+
+//create model for the project
+//collection - users
+const User = mongoose.model('User',{
+    username:String,
+    acno:Number,
+    password:String,
+    balance:Number,
+    transaction:[]
+})
+
+//export model
+module.exports={
+    User
+}
